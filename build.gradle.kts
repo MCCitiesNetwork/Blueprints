11a2b519fdb61e164bcefd5f2de736df2062be21
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2026-05-09 22:16:28 +1000

    kotlin dsl buildscripts

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    java
+}
+
+group = "io.github.bl3rune"
+version = "1.7.5"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    implementation("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+    implementation("com.google.code.gson:gson:2.12.0")
+    annotationProcessor(group = "org.spigotmc", name = "plugin-annotations", version = "1.3-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.yaml:snakeyaml:2.2")
+    testImplementation("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
